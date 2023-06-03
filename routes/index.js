86ecfb660f8c4de08f26aeb2140b66e83f1e1d09
Rheinma <aderhendra04@gmail.com>
2023-06-03 13:58:41 +0700

    Feat:Bodyparts Endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const Controller = require("../controllers/gymsController");
+
+router.get("/pub/bodyparts/", Controller.allBodyParts);
+router.get("/pub/bodyparts/:id", Controller.getBodyPartsById);
 
 router.get('/', (req, res) => {
   res.send('hello world');
