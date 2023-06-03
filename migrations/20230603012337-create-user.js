cff7607a17e81b262932825bd7f561508d8f04b0
Itan Sutarlan <sutarlanitan@gmail.com>
2023-06-03 13:03:22 +0700

    create user entity and its testing

---
+++ migrations/20230603012337-create-user.js
@@ -0,0 +1,57 @@
+/* istanbul ignore file */
+/* eslint-disable */
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      gender: {
+        type: Sequelize.ENUM({
+          values: ['Male', 'Female'],
+        }),
+        allowNull: true,
+      },
+      role: {
+        type: Sequelize.ENUM({
+          values: ['Client', 'Admin'],
+        }),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,111 @@
+/* istanbul ignore file */
+/* eslint-disable */
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'Username must be unique'
+      },
+      validate: {
+        notEmpty: {
+          msg: 'Username is required',
+        },
+        notNull: {
+          msg: 'Username is required',
+        },
+        is: {
+          args: /^[\w]+$/,
+          msg: 'Username contain restricted character'
+        },
+        len: {
+          args: [2,50],
+          msg: 'Password must have length between 2 and 50'
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'Email must be unique'
+      },
+      validate: {
+        notEmpty: {
+          msg: 'Email is required',
+        },
+        notNull: {
+          msg: 'Email is required',
+        },
+        isEmail: {
+          msg: 'Invalid email format',
+        }
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Password is required',
+        },
+        notNull: {
+          msg: 'Password is required',
+        },
+        len: {
+          args: [6,20],
+          msg: 'Password must have length between 6 and 20'
+        },
+      },
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: {
+          args: [2,20],
+          msg: 'FullName must have length between 2 and 20'
+        },
+      },
+    },
+    gender: {
+      type: DataTypes.ENUM({
+        values: ['Male', 'Female'],
+      }),
+      allowNull: true,
+    },
+    role: {
+      type: DataTypes.ENUM({
+        values: ['Client', 'Admin'],
+      }),
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: 'Role is required',
+        },
+        notNull: {
+          msg: 'Role is required',
+        },
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
